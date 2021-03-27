6d3dc4da45a649f7b42e177be4fb03af8e91cf3d
Vamsi Krishna <vamsibk1997@gmail.com>
2021-03-27 19:56:44 +0530

    Create FindFirstMissingPositive.java
    This problem tries to figure out the first missing positive number in the given array. The current solution is of O(nlogn) + O(n) as time complexity.
    
    problem link: https://www.interviewbit.com/problems/first-missing-integer/#

---
+++ Arrays/FindFirstMissingPositive.java
@@ -0,0 +1,50 @@
+package com.practice.java.check;
+
+import java.util.Arrays;
+
+public class FindFirstMissingPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {-2,10,-5,9,8,6};
+		System.out.println(firstMissingPositive(arr));
+	}
+
+	public static int firstMissingPositive(int[] A) {
+		// 1 2 0 ---> 3
+		// 3 4 1 -1 ---> 2
+		//-6 -7 -8 ----> 1
+
+		int first = 1;
+		int firstIdx = Integer.MIN_VALUE;
+		int len = A.length;
+		Arrays.sort(A);
+
+		for(int i=0;i<len;i++)
+		{
+			if(A[i] > 0)
+			{
+				firstIdx = i;
+				break;
+			}
+		}
+
+		if(firstIdx == Integer.MIN_VALUE){
+			return 1;
+		}
+		for(int i=firstIdx;i<len;i++)
+		{
+			if(first == A[i])
+			{
+				first++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return first;
+	}
+
+}
